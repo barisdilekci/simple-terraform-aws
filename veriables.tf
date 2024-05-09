fba9df2272f08998d2101ad3d3de153fcbc56330
BarisDilekci <bdilekci200@gmail.com>
2024-05-10 00:27:53 +0300

    terraform

---
+++ veriables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+    default = "us-west-2"
+}
+variable "access_key" {
+    default = "enter your access_key"
+    
+}
+variable "secret_key" {
+  default = "enter your secret_key"
+}
